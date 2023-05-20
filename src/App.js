90d34283ead4f2c85df5a9e8f4c396a515d6ee30
skgithubuser <shruti300000@gmail.com>
2023-05-20 12:25:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './Search.css';
+const Search = () => {
+  const [searchText, setSearchText] = useState('');
+  const [recentSearches, setRecentSearches] = useState([]);
 
-function App() {
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  const handleSearch = () => {
+    if (searchText.trim() === '') {
+      return;
+    }
+
+    const lowerCaseSearchText = searchText.toLowerCase();
+
+    // Check if the search text already exists in recent searches
+    const existingIndex = recentSearches.findIndex(search => search.toLowerCase() === lowerCaseSearchText);
+
+    if (existingIndex !== -1) {
+      // If the search text exists, remove it from its current position
+      const updatedSearches = [
+        recentSearches[existingIndex],
+        ...recentSearches.slice(0, existingIndex),
+        ...recentSearches.slice(existingIndex + 1)
+      ];
+      setRecentSearches(updatedSearches);
+    } else {
+      // Add the search text to the top of recent searches
+      const updatedSearches = [searchText, ...recentSearches.slice(0, 9)];
+      setRecentSearches(updatedSearches);
+    }
+
+    // Clear the search input
+    setSearchText('');
+  };
+
+  const handleDeleteSearch = (search) => {
+    // Delete the search from recent searches
+    const updatedSearches = recentSearches.filter(item => item !== search);
+    setRecentSearches(updatedSearches);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={searchText}
+        onChange={handleSearchChange}
+      />
+      <button onClick={handleSearch}>Search</button>
+      <ul>
+        {recentSearches.map((search, index) => (
+          <li key={index}>
+            <span>{search}</span>
+            <button onClick={() => handleDeleteSearch(search)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default Search;
